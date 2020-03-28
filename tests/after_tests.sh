9b5f8359c45f746a7c180ab3071c2d3d6446cd45
luigi.1809@gmail.com <luigi.1809@gmail.com>
2020-03-28 16:06:39 +0100

    add travis

--- tests/after_tests.sh
+++ tests/after_tests.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 google=$(cat /tmp/google)
-google=173.194.74.147
 
 # test with forcing IP HTTP
 echo test with forcing IP HTTP
-[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" http://$google/ -H "Host: www.google.com")" == "200" ] && grep http://www.google.com /var/log/webfilter-ng | tail -n1 | grep DROP && exit 1
+[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.com:80:$google http://www.google.com/)" == "200" ] && exit 1
 grep "http://www.google.com" /var/log/webfilter-ng | tail -n1 | grep DROP || exit 1
 
+sleep 5
 
 # test with forcing IP HTTPS
 echo test with forcing IP HTTPS
-[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.fr:80:$google http://www.google.com/)" == "200" ] && exit 1
+[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.com:443:$google https://www.google.com/)" == "200" ] && exit 1
 grep https://www.google.com /var/log/webfilter-ng | tail -n1 | grep DROP || exit 1
 
+sleep 5
 
 # test without forcing IP HTTP
 echo test without forcing IP HTTP
 [ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" http://www.google.com/)" == "200" ] || exit 1
 grep http://www.google.com /var/log/webfilter-ng | tail -n1 | grep ACCEPT || exit 1
+
+sleep 5
 
 
 # test without forcing IP HTTPS
--- tests/before_tests.sh
+++ tests/before_tests.sh
@@ -6,11 +6,11 @@ google=$(cat /tmp/google)
 
 curl 
 echo test with forcing IP HTTP
-[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.fr:80:$google http://www.google.com/)" == "200" ] || exit 1
+[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.com:80:$google http://www.google.com/)" == "200" ] || exit 1
 
 # test with forcing IP HTTPS
 echo test with forcing IP HTTPS
-[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.fr:443:$google https://www.google.com/)" == "200" ] || exit 1
+[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" --resolve www.google.com:443:$google https://www.google.com/)" == "200" ] || exit 1
 
 # test without forcing IP HTTP
 echo test without forcing IP HTTP
