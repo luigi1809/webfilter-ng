980f44c05812471e083a6cb1c645c923f70c383f
luigi.1809@gmail.com <luigi.1809@gmail.com>
2020-03-28 15:24:45 +0100

    add travis

--- tests/after_tests.sh
+++ tests/after_tests.sh
@@ -2,20 +2,24 @@
 google=$(cat /tmp/google)
 
 # test with forcing IP HTTP
+echo test with forcing IP HTTP
 [ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" http://$google/ -H "Host: www.google.com")" == "200" ] && exit 1
 grep google.com /var/log/webfilter-ng | tail -n1 | grep DROP || exit 1
 
 
 # test with forcing IP HTTPS
+echo test with forcing IP HTTPS
 [ "$(curl -k --max-time 5 -s -o /dev/null -w "%{http_code}" https://$google/ -H "Host: www.google.com")" == "200" ] && exit 1
 grep google.com /var/log/webfilter-ng | tail -n1 | grep DROP || exit 1
 
 
 # test without forcing IP HTTP
+echo test without forcing IP HTTP
 [ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" http://www.google.com/)" == "200" ] || exit 1
 grep google.com /var/log/webfilter-ng | tail -n1 | grep ACCEPT || exit 1
 
 
 # test without forcing IP HTTPS
+echo test without forcing IP HTTPS
 [ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" https://www.google.com/)" == "200" ] || exit 1
 grep google.com /var/log/webfilter-ng | tail -n1 | grep ACCEPT || exit 1
