f4cea882b7b81a96f26876d74540d1f16d7de030
luigi.1809@gmail.com <luigi.1809@gmail.com>
2020-03-28 15:39:59 +0100

    add travis

--- tests/after_tests.sh
+++ tests/after_tests.sh
@@ -3,13 +3,13 @@ google=$(cat /tmp/google)
 
 # test with forcing IP HTTP
 echo test with forcing IP HTTP
-[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" http://$google/ -H "Host: www.google.com")" == "200" ] && exit 1
+[ "$(curl --max-time 5 -s -o /dev/null -w "%{http_code}" http://$google/ -H "Host: www.google.com")" == "200" ] && grep google.com /var/log/webfilter-ng | tail -n1 | grep DROP && exit 1
 grep google.com /var/log/webfilter-ng | tail -n1 | grep DROP || exit 1
 
 
 # test with forcing IP HTTPS
 echo test with forcing IP HTTPS
-[ "$(curl -k --max-time 5 -s -o /dev/null -w "%{http_code}" https://$google/ -H "Host: www.google.com")" == "200" ] && exit 1
+[ "$(curl -k --max-time 5 -s -o /dev/null -w "%{http_code}" https://$google/ -H "Host: www.google.com")" == "200" ] && grep google.com /var/log/webfilter-ng | tail -n1 && curl -k --max-time 5 -s -o /dev/null -w "%{http_code}" https://$google/ -H "Host: www.google.com" && exit 1
 grep google.com /var/log/webfilter-ng | tail -n1 | grep DROP || exit 1
 
 
